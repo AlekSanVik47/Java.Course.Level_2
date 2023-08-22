d6c601d8600b3557ecf28d418ec7145ec68ca6fa
Александр <aleksandrvikbozhko@gmail.com>
2023-08-22 10:43:15 +0400

    добавил варианты подсчета дубликатов

--- Task/Lesson_3/Words.java
+++ Task/Lesson_3/Words.java
@@ -1,23 +1,45 @@
 package Task.Lesson_3;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
 
 public class Words {
-
-    public static void main(String[] args) {
-        String[] words = {"Друзья", "Взгляд", "Глаза",
-                "Игра", "Улыбка", "Радость", "Ученье", "Жизнь", "Игра", "Улыбка",
-                "Радость", "Ученье", "Смех", "Успех", "Школа", "Доллар", "Золото",
-                "Погода"};
-
+    public Set<String> getUniqueWords(String[] arrayOfWords){
+        return new HashSet<>(asList(arrayOfWords));
+    }
+    public void getNumberOfRepetitions(String[] arrayOfWords){
+        Arrays.stream(arrayOfWords)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(element -> element.getValue() >= 1 )
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+    public Map<String, Integer> countingWordsWithMap(String[] words) {
         Map<String, Integer> map = new HashMap<>();
         for (String word : words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
-        System.out.println(map);
+        return map;
     }
+    public static void main(String[] args) {
+        String[] words = {"Друзья", "Взгляд", "Глаза",
+                "Игра", "Улыбка", "Радость", "Ученье", "Жизнь", "Игра", "Улыбка",
+                "Радость", "Ученье", "Смех", "Успех", "Школа", "Доллар", "Золото",
+                "Погода", "Ученье"};
+        Words newWords = new Words();
+        System.out.printf("В массиве %d уникальных слов \nСписок уникальных слов: %s%n", newWords.getUniqueWords(words).size(), newWords.getUniqueWords(words));
+        System.out.println("__________________подсчет дубликатов с помощью Stream api_______________");
+        newWords.getNumberOfRepetitions(words);
+        System.out.println("__________________подсчет дубликатов с помощью Map________________");
+        System.out.println(newWords.countingWordsWithMap(words));
+    }
+
+
 
 
 }
