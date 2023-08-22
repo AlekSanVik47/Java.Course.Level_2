26708df8a58f857a10654530ba5d2299b94c85fb
Александр <aleksandrvikbozhko@gmail.com>
2023-08-22 10:43:15 +0400

    вывел уникальные слова

--- Task/Lesson_3/Words.java
+++ Task/Lesson_3/Words.java
@@ -1,16 +1,20 @@
 package Task.Lesson_3;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Words {
-
+    public Set<String> getUniqueWords(String[] arrayOfWords){
+        Set<String> uniqueWords = new HashSet<>(Arrays.asList(arrayOfWords));
+        return uniqueWords;
+    }
     public static void main(String[] args) {
         String[] words = {"Друзья", "Взгляд", "Глаза",
                 "Игра", "Улыбка", "Радость", "Ученье", "Жизнь", "Игра", "Улыбка",
                 "Радость", "Ученье", "Смех", "Успех", "Школа", "Доллар", "Золото",
                 "Погода"};
+        Words newWords = new Words();
+        System.out.printf("В массиве %d уникальных слов \nСписок уникальных слов: %s%n", newWords.getUniqueWords(words).size(), newWords.getUniqueWords(words));
+
 
         Map<String, Integer> map = new HashMap<>();
         for (String word : words) {
